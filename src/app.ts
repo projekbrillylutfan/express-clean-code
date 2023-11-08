9a9964b9cd87511f5201888f5c7d8ab26b8e7f75
projekbrillylutfan <projekbrillylutfan@gmail.com>
2023-11-08 15:37:52 +0700

    penambahan kodingan di src handler, data, mode(dto dan entity

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log("mencoba")
+import express, { Application, NextFunction } from "express";
+import UserHandler from "./handlers/users";
+//import PostHandler from "./handlers/users";
+
+const app: Application = express();
+const PORT: number = 8080;
+
+app.use(express.json());
+
+const userHandler = new UserHandler();
+// const postHandler = new PostHandler();
+
+
+
+app.get("/api/users", userHandler.getUsers);
+app.get("/api/users/:id", userHandler.getUserById);
+app.post("/api/users", userHandler.creatUser);
+app.delete("/api/users/:id", userHandler.deleteUserById);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
